f75cc8f65507b0f6e1df29bf8026341bb0f2c510
munzembaesdrascom <82828707+munzembaesdrascom@users.noreply.github.com>
2022-07-09 18:48:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Aceuil</title>
+</head>
+<body>
+   <a href="pages\insert.php">calcul</a>
+   <a href="pages\history.php">historique</a>
+   <a href="pages\historiq.php">historique css</a>
+</body>
+</html>
